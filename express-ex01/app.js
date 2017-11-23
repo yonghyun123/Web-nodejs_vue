a5e9cde3e613bdefa4968dbee6447593e15884fc
yonghyun-kim <model9960@naver.com>
2017-11-23 15:51:56 +0900

    Local Component vs Global Component

--- express-ex01/app.js
+++ express-ex01/app.js
@@ -13,6 +13,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// view engine convert to html
 app.engine('html',require('ejs').renderFile);
 app.set('view engine', 'html');
 
@@ -25,7 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/uer', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
